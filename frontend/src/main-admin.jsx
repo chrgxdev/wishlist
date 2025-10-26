6874e7d75c84cf45e99293175b6b75c380a63736
user01 <me@chris09.net>
2025-10-26 19:22:52 +0800

    commmit123

--- frontend/src/main-admin.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AdminPage from './pages/Admin.jsx';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<AdminPage />);
